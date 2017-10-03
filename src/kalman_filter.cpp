f019cd874d2f4d831cb45113ff64745c983bcb95
CJHMPower <1120798947@qq.com>
2017-10-03 17:11:18 +0800

    first commit

---
+++ src/kalman_filter.cpp
@@ -0,0 +1,76 @@
+#include "kalman_filter.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+  TODO:
+    * predict the state
+  */
+  x_=F_*x_;
+  MatrixXd Ft=F_.transpose();
+  P_=F_*P_*Ft+Q_;
+  
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+	//new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+    VectorXd z_pred=VectorXd(3);
+    float r= sqrt(x_(0)*x_(0)+x_(1)*x_(1));
+    float phi=3.1415926;
+    z_pred<<r,atan2(x_(1),x_(0)), (x_(0)*x_(2)+x_(1)*x_(3))/r;
+    VectorXd y=z-z_pred;
+    if(y(1)<-phi){
+      y(1)=y(1)+2*phi;
+    }
+    else if(y(1)>phi){
+      y(1)=y(1)-2*phi;
+    }
+    MatrixXd Ht=H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
+}
